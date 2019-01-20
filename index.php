6ee75157548e6ace39c21b2a8e591b0fa80499c8
Matthew Barry <mbarry5@mail.greenriver.edu>
2019-01-20 13:20:23 -0500

    added default route to display home page

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
 <?php
+	/**
+	 * @author Matthew Barry
+	 * @version 1.0
+	 *
+	 * This file displays the home page via a view default route
+	 */
+	//turn on error reporting
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
+
+	require_once('vendor/autoload.php');
+
+	//fat free framework
+	$f3=Base::instance();
+
+	// turn on fet-free error reporting
+	$f3->set('DEBUG',3);
+
+	//define a default route
+	$f3->route('GET /',
+		function()
+		{
+			$view=new View;
+			echo $view->render('views/home.html');
+		});
+
+	//run fat free
+	$f3->run();
+
